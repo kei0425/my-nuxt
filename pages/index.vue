6322e351dfe541c8a3175402bd94f70a1c389a6d
岡田圭介 <k.okada@econavista.com>
2020-11-01 07:41:58 +0000

    todo UIのみ

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,71 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        my-nuxt
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <h1>ToDo リスト</h1>
+    <table>
+      <tr>
+        <th>連番</th><th>内容</th><th>完了</th><th>追加日</th><th>操作</th>
+      </tr>
+      <tr v-for="(todo, i) in todos" :key="i">
+        <td>{{i + 1}}</td>
+        <td>{{todo.title}}</td>
+        <td><input type="checkbox" :checked="todo.done" @change="toggle(i)" />{{todo.done}}</td>
+        <td>{{format(todo.created_at)}}</td>
+        <td><button @click.prevent="remove(i)">削除</button></td>
+      </tr>
+      <tr>
+        <td>新規</td>
+        <td><input type="text" v-model="title" /></td>
+        <td></td>
+        <td></td>
+        <td><button @click.prevent="add">追加</button></td>
+      </tr>
+    </table>
+    {{title}}
   </div>
 </template>
 
 <script>
-export default {}
+export default { // 他のファイルから参照できるようにする指定
+  data() {
+    return {
+      todos: [
+        { title: '歯磨き', done: true, created_at: new Date() },
+        { title: '宿題', done: false, created_at: new Date() }
+      ],
+      title: null,
+    }
+  },
+  methods: {
+    format(date) {
+      return date.getFullYear()
+        + '/' + (date.getMonth() + 1)
+        + '/' + date.getDate() 
+        + ' ' + date.getHours()
+        + ':' + date.getMinutes();
+    },
+    add() {
+      if (this.title != null) {
+        this.todos.push({
+          title: this.title,
+          done: false,
+          created_at: new Date()
+        });
+      }
+      this.title = null;
+    },
+    remove(i) {
+      this.todos.splice(i, 1);
+    },
+    toggle(i) {
+      // this.$set(this.todos[i], 'done', !this.todos[i].done);
+      this.todos[i].done = !this.todos[i].done;
+    }
+  }
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+th, td {
+  border: 1px solid silver;
 }
 </style>
